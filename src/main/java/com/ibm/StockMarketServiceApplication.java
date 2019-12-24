e52cbaa1bbddc31e05018b737cc3808b2cbb8387
AI PING YU <yuaip@cn.ibm.com>
2019-12-24 14:56:09 +0800

    second version

--- src/main/java/com/ibm/StockMarketServiceApplication.java
+++ src/main/java/com/ibm/StockMarketServiceApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableDiscoveryClient
 @RestController
 @SpringBootApplication
+@EnableSwagger2
 public class StockMarketServiceApplication {
 	
 	public static void main(String[] args) {
